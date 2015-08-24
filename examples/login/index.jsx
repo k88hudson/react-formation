73ae0d865e6c4dc7826883de7acc07835427376c
Kate Hudson <k88hudson@gmail.com>
2015-08-24 11:19:05 -0400

    trying blur stuff

--- examples/login/index.jsx
+++ examples/login/index.jsx
@@ -34,8 +34,20 @@ var Form = CreateForm({
       type: 'boolean'
     }
   },
+  getInitialState: function () {
+    return {
+      blurred: {}
+    };
+  },
   onSuccess: function (data) {
     alert(JSON.stringify(data));
+  },
+  onBlur: function (field) {
+    return () => {
+      var blurred = this.state.blurred;
+      blurred[field] = true;
+      this.setState({blurred});
+    }
   },
   getPassStrength: function () {
     return this.state.password ? zxcvbn(this.state.password).score : -1;
@@ -43,27 +55,26 @@ var Form = CreateForm({
   passStrengthStrings: ['Not strong enough', 'Weak', 'OK', 'Good', 'Strong'],
   render: function () {
     var passStrength = this.getPassStrength();
-
     return (<form className="login-eg">
 
       <div className="body">
         <div className="form-group">
-          <input placeholder="Username" type="text" valueLink={this.linkField('username')} />
-          <ErrorMessage field="username" />
+          <input placeholder="Username" type="text" onBlur={this.onBlur('username')} valueLink={this.linkField('username')} />
+          <ErrorMessage show={this.state.blurred.username} field="username" />
         </div>
 
         <div className="form-group">
-          <input placeholder="Email" type="text" valueLink={this.linkField('email')} />
-          <ErrorMessage field="email" />
+          <input placeholder="Email" type="text" onBlur={this.onBlur('email')} valueLink={this.linkField('email')} />
+          <ErrorMessage show={this.state.blurred.email} field="email" />
         </div>
 
         <div className="form-group">
-          <input placeholder="Password" type="password" name="password" valueLink={this.linkField('password')} />
+          <input placeholder="Password" type="password" name="password" onBlur={this.onBlur('password')} valueLink={this.linkField('password')} />
           <div className={'password-strength strength-' + passStrength}>
             {passStrength >= 0 && this.passStrengthStrings[passStrength]}
             <span className="indicator" style={{width: ((passStrength + 1) * 20) + '%'}} />
           </div>
-          <ErrorMessage field="password" />
+          <ErrorMessage show={this.state.blurred.password} field="password" />
         </div>
       </div>
 
