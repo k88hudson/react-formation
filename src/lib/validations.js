16ddb0fdb3bcc1305183935751510579b0985355
Kate Hudson <k88hudson@gmail.com>
2015-08-24 11:02:06 -0400

    oops lint

--- src/lib/validations.js
+++ src/lib/validations.js
@@ -1,8 +1,6 @@
 var EMAIL_REGEX = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
 var NUMBER_REGEX = /^\d+$/;
 
-var assign = require('react/lib/Object.assign');
-
 // TODO: make configurable, localizable, add basic templating
 var messages = {
   email: 'This must be a valid email',
